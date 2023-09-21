f9d0e9a19260fbb8badc6ba80f063b17a62536cc
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-09-21 11:16:07 +0530

    Question2

---
+++ Question2.java
@@ -0,0 +1,10 @@
+public class Question2
+ {
+    public static void main(String[] args)
+     {
+        int a = 5;
+        int b = 6;
+        int sum = a +b;
+        System.out.println("Sum of a and b is : " +sum);
+    }
+}
