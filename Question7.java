e83a6d4fae0ddc2b8371b760e3d5fce06391a552
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-09-22 17:50:34 +0530

    SWapping

---
+++ Question7.java
@@ -0,0 +1,21 @@
+//WAP to swap two numbers .
+public class Question7 {
+    public static void main(String args[])
+    {
+        int a = 8;
+        int b =23;
+        System.out.println("Numbers before swapping : ");
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+
+        int c ;
+        c = a;
+        a = b;
+        //b = c;
+
+        System.out.println("After swapping numbers are : ");
+        System.out.println("a : "+a);
+        System.out.println("b : "+b);
+        //System.out.println("c : "+c);
+    }
+}
