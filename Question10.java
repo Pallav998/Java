1cd0be5dd196d5d19899c9873d9e2be98e361579
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-09-23 17:18:30 +0530

    Natural Number

---
+++ Question10.java
@@ -0,0 +1,12 @@
+//Write a program to calculate the sum of first 10 natural number.
+public class Question10 {
+    public static void main(String args[])
+    {
+        int sum =0;
+        for(int i=0;i<=10;i++)
+        {
+            sum += i;
+        }
+        System.out.println("Sum: " + sum);
+    }
+}
