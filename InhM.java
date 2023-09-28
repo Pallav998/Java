5d5bc79ad19d483a1bb4e3f0ae10f6892301842a
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-09-28 11:54:34 +0530

    Multiple inheritance

---
+++ InhM.java
@@ -0,0 +1,71 @@
+
+// Interface for color
+interface Color {
+    String getColor();
+}
+
+class Tree {
+    String type;
+
+    public Tree(String type) {
+        this.type = type;
+    }
+
+    public void grow() {
+        System.out.println(type + " tree is growing.");
+    }
+}
+
+// Child class 1
+class Grapes extends Tree implements Color {
+    String color;
+
+    public Grapes(String type, String color) {
+        super(type);
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void produceJuice() {
+        System.out.println("Making " + color + " grapes juice.");
+    }
+}
+
+// Child class 2
+class Papaya extends Tree implements Color {
+    String color;
+
+    public Papaya(String type, String color) {
+        super(type);
+        this.color = color;
+    }
+
+    @Override
+    public String getColor() {
+        return color;
+    }
+
+    public void sliceAndEat() {
+        System.out.println("Slicing and eating " + color + " papaya.");
+    }
+}
+
+
+
+public class InhM {
+    public static void main(String[] args) {
+        Grapes grapes = new Grapes("Grapes", "Purple");
+        Papaya papaya = new Papaya("Papaya", "Orange");
+
+        grapes.grow();
+        System.out.println("Grapes color: " + grapes.getColor());
+        grapes.produceJuice();
+
+        papaya.grow();
+        System.out.println("Papaya color: " + papaya.getColor());
+        papaya.sliceAndEat();
+    }
+}
