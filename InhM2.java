554a09760b3eec30054f4aa492b6c753e9510bfd
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-09-28 18:20:50 +0530

    Example 2

---
+++ InhM2.java
@@ -0,0 +1,36 @@
+//Basic example of multiple inheritance.
+interface A
+{
+    void Show();
+}
+
+ interface B
+ {
+     void Display();
+ }
+
+
+ class Multiple implements A, B
+ {
+   public void Show()
+   {
+    System.out.println("Interface A is here ");
+   }
+   public void Display()
+   {
+    System.out.println("Interface  B is here");
+   }
+   
+}
+   public class InhM2
+   {
+    public static void main(String args[])
+    {
+    Multiple m = new Multiple();
+    m.Show(); 
+    m.Display();
+    }
+}
+
+
+
