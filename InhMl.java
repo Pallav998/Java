503121b2e5f598d93516590a924fa15e0d886207
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-09-28 18:50:54 +0530

    Multilevel

---
+++ InhMl.java
@@ -0,0 +1,53 @@
+class A {
+    int a, b, c;
+
+    void add() {
+        a = 12;
+        b = 8;
+        c = a + b;
+        System.out.println(" Sum of two numbers is: " + c);
+    }
+
+    void sub() {
+        a = 12;
+        b = 8;
+        c = a - b;
+        System.out.println(" Subtraction of two numbers is: " + c);
+    }
+}
+
+class B extends A {
+    void multy() {
+        a = 12;
+        b = 8;
+        c = a * b;
+        System.out.println(" Multiplication of two numbers is: " + c);
+    }
+
+    void div() {
+        a = 12;
+        b = 8;
+        c = a / b;
+        System.out.println(" Division of two numbers is: " + c);
+    }
+}
+
+class C extends B {
+    void rem() {
+        a = 12;
+        b = 8;
+        c = a % b;
+        System.out.println(" Remainder of two numbers is: " + c);
+    }
+}
+
+public class InhMl {
+    public static void main(String[] args) {
+        C r = new C();
+        r.add();
+        r.sub();
+        r.multy();
+        r.div();
+        r.rem();
+    }
+}
