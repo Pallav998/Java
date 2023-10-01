09a3971ebc735123e701e8988d8810f721dff9b9
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-01 12:33:03 +0530

    Logical Operator

---
+++ L4g.java
@@ -0,0 +1,12 @@
+//WAP for Logical Operator example .
+public class L4g {
+    public static void main(String args[])
+    {
+        int a=5,b=1005;
+
+        System.out.println("And  &&  : "+(a>=98 && b>=(8)));
+
+        System.out.println("Or   ||  : "+(a>=98 || b>=98));
+ 
+    }
+}
