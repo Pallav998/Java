f92db3784df33a71c658d9394ea7bdaa116b11c3
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-01 12:26:27 +0530

    Conditional Operator

---
+++ L4f.java
@@ -0,0 +1,13 @@
+//WAP for conditional operator where two integer variables a and b values will be given , check which one is greater then print the greatest value
+public class L4f {
+    public static void main(String args[])
+    {
+        int a=45,b=75,c;
+
+        c=a>b?a:b; //Condition? expression1: expression2; this is the syntax.
+
+
+        System.out.println("The Greatest Number is : "+c);
+ 
+    } 
+}
