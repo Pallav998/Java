e9458b6904347865fabbfb15e8eef891acd68b19
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-05 23:18:25 +0530

    Triangle

---
+++ P3.java
@@ -0,0 +1,14 @@
+//WAP to print a side triangle
+public class P3 {
+    public static void main(String[] args) {
+        
+        int size = 5;
+        for (int i = 0; i < size; i++) {
+          for (int j = 0; j <= i; j++)
+           {
+            System.out.print(" * ");
+          }
+          System.out.println();
+        }
+      }
+}
