30e563b8bfb0d31dedb7d20b0cf20a9933c0f41e
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-09 10:39:16 +0530

    Control Statement 2

---
+++ C2.java
@@ -0,0 +1,11 @@
+//Control Statement :
+//WAP to print first 10 number in reverse order by using loop statement.
+public class C2 {
+    public static void main(String[] args) {
+     
+        for(int i =10; i>=1; i--)
+        {
+            System.out.println(i);
+        }
+    }
+}
