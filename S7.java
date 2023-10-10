631193ecf0c77ba1c8129c83494f480da4ded6ed
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-10 19:16:15 +0530

    Comparision of the string

---
+++ S7.java
@@ -0,0 +1,15 @@
+//WAP for comparision of two strings.
+public class S7 {
+    public static void main(String[] args) {
+        String A = "Pallavi";
+        String B = "pallavi";
+
+        if(A.compareTo(B)==0)
+        {
+            System.out.println(" Strings are equal .");
+        }
+        else{
+            System.out.println("Strings are not equal ");
+        }
+    }
+}
