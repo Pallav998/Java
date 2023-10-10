3e67e541b8671b728ebb9d2ded3821a4f89cdf56
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-10 19:09:50 +0530

    Substring

---
+++ S6.java
@@ -0,0 +1,10 @@
+//WAP to print the substring of the string.
+public class S6 {
+    public static void main(String[] args) {
+        String S ="Pallavi" ;
+        String sub = S.substring(3,7);
+        System.out.println(sub);
+        String subs = S.substring(3);
+        System.out.println(subs);
+    }
+}
