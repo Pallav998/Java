0ccbb5caf8c1a1e4ba8a783f3cc4cfb2b06d5c83
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-10 18:31:02 +0530

    Concatenation

---
+++ S3.java
@@ -0,0 +1,9 @@
+//WAP to concatenate two strings.
+public class S3 {
+    public static void main(String[] args) {
+        String F_name = "Pallavi";
+        String L_name = "Thakur";
+        String Full_name = F_name + " "+ L_name;
+        System.out.println("COncatenate String is : "+Full_name);
+    }
+}
