07458bfe38de28578399f497a699cdaecdd171e4
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-10 18:39:59 +0530

    Length of string

---
+++ S4.java
@@ -0,0 +1,14 @@
+//WAP to find the length of the string .
+import java.util.Scanner;
+public class S4 {
+  public static void main(String[] args)
+   {
+    
+    Scanner s =new Scanner(System.in);
+    System.out.print("Enter the string : ");
+        String S = s.nextLine();
+
+        System.out.println(" Your String is : "+S);
+        System.out.println(" Length of the string is : "+S.length());
+  }  
+}
