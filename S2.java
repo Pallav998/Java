ff9b4e844a02de2658ac04f557918b0297bf6e76
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-10 17:59:07 +0530

    String User Input

---
+++ S2.java
@@ -0,0 +1,11 @@
+//WAP where user input string.
+import java.util.Scanner;
+public class S2 {
+    public static void main(String[] args) {
+        System.out.print("Enter a string Name ");
+        Scanner s =new Scanner(System.in);
+        String name = s.next();
+
+        System.out.println("Your name is : "+name);
+    }
+}
