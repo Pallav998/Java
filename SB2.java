2b1f5d5f6a8606aab99ae06c461dd1a6bb0beecc
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-11 21:42:40 +0530

    String Builder

---
+++ SB2.java
@@ -0,0 +1,9 @@
+//Basic example of string builder class  indexing.
+public class SB2 {
+   public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder("Dhoni");
+    System.out.println(sb);
+
+    System.out.println(sb.charAt(1));
+   } 
+}
