bb60ed47aba037291c562a3d476d7354f3e5069b
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-11 21:39:20 +0530

    StringBuilder

---
+++ SB1.java
@@ -0,0 +1,7 @@
+//Basic example of string builder class .
+public class SB1 {
+   public static void main(String[] args) {
+    StringBuilder sb = new StringBuilder("Dhoni");
+    System.out.println(sb);
+   } 
+}
