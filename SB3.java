5d982be20c5018480361498d2589c8ae072fc336
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-11 21:46:19 +0530

    String builder setchar

---
+++ SB3.java
@@ -0,0 +1,10 @@
+//WAP to set chat at index 2 a .
+public class SB3 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Dhoni");
+        System.out.println(sb);
+    
+        sb.setCharAt(2, 'a');
+        System.out.println(sb);
+       } 
+}
