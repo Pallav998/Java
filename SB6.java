75b10a3d8236cbea469458ee48dba20993f12f6a
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-11 22:07:18 +0530

    Append

---
+++ SB6.java
@@ -0,0 +1,14 @@
+//WAP to add new char in the end of the string.
+public class SB6 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("MS");
+        System.out.println(sb);
+        sb.append('D');
+        sb.append('h');
+        sb.append('o');
+        sb.append('n');
+        sb.append('i');
+
+        System.out.println(sb);
+    }
+}
