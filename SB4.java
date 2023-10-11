b78b4a71b17534053bebe61af6bd4da1a52ff0cc
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-11 21:57:01 +0530

    Insert

---
+++ SB4.java
@@ -0,0 +1,11 @@
+//WAP to insert a char in the  string  .
+public class SB4 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Dhoni");
+        System.out.println(sb);
+    
+        sb.insert(2,'o');
+        System.out.println(sb);
+       } 
+}
+
