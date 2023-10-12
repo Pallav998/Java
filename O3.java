bd39363709c0f8359f9198632afb94ba49bf1826
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-12 12:21:03 +0530

    Logical Operator

---
+++ O3.java
@@ -0,0 +1,10 @@
+//Logical Operator :
+public class O3 {
+    public static void main(String[] args) {
+        boolean a =true;
+        boolean b=false ;
+        System.out.println("a && b is : "+(a && b)); // It will perform and operation T T =T
+        System.out.println("a || b is : "+(a || b));// It will perform Or operation  F F =F otherwise true
+        System.out.println("!(a = b) = " + !(a =b));// It will perform not operation
+    }
+}
