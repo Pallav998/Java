950df86eaa568df6a20c87dc426eb621e38c740a
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-12 11:36:15 +0530

    Relational Operator

---
+++ O2.java
@@ -0,0 +1,35 @@
+//Relationship Operator it will return true of false.
+public class O2 {
+   public static void main(String[] args) {
+    int a =98;
+    int b =84;
+
+    if(a==b)
+    {
+        System.out.println(a==b);
+    }
+    else if(a!=b)
+    {
+        System.out.println(a!=b);
+    }
+    else if(a<b)
+    {
+        System.out.println(a<b);
+    }
+    else if(a>b)
+    {
+        System.out.println(a>b);
+    }
+    else if(a<=b)
+    {
+        System.out.println(a<=b);
+    }
+    else if (a>=b)
+    {
+        System.out.println(a>=b);
+    }
+    else{
+        System.out.println("Nothing");
+    }
+   } 
+}
