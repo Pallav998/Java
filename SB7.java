d6543bd591cfbcf9015f74ca289a2d9e268f964d
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-10-12 10:48:53 +0530

    String builder set

---
+++ SB7.java
@@ -0,0 +1,19 @@
+public class SB7 {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder("Hello");
+
+        for(int i =0; i<sb.length()/2;i++)
+        {
+            int front = i;
+            int back = sb.length() -i-1;
+            System.out.println(sb);
+
+            char frontchar = sb.charAt(front);
+            char backchar = sb.charAt(back);
+
+            sb.setCharAt(front, backchar);
+            sb.setCharAt(back, frontchar);
+        }
+        System.out.println(sb);
+    }
+}
