28c42864f783188da7a5c6db4b22c4f43038b854
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-09 11:42:44 +0530

    print array elements

---
+++ Practice/A1.java
@@ -0,0 +1,17 @@
+//WAP to print array elements.
+
+import java.util.Arrays;
+
+public class A1 {
+    public static void main(String[] args) {
+        int a[] = {2,3,4,5,6,7} ;
+
+        System.out.println("The array elements are : ");
+
+        for(int i = 0 ; i < a.length ;i++)
+        {
+            System.out.print(a[i]+ " ");
+        }
+        System.out.println();
+    }
+}
