9545afb6f93c72079194c8bfe3f014f2d188044a
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-12 12:21:53 +0530

    Basic

---
+++ Basics/Q1.java
@@ -0,0 +1,19 @@
+//Explain Public static void main:
+
+public class Q1 {
+   public static void main(String[] args) {
+    System.out.println("Hello Java");
+    
+   } 
+}
+// here public is an access modifier which can be accessed by any other class.
+
+//static is a keyword .
+//when JVM calls the main method there is no object of the class present so main method needs to be static.
+
+//void is the return type. It indicates that the main method not return anything.It simply perform some actions
+
+//main is the name of the method.without the main method JVM will not execute the program.
+
+//string args[]: It is a method parameter.
+// JVM always look for the main method string type with array as a parameter.
