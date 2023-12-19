a0e94db76fb1fd4b46961dbadb6fc60a424ba29c
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-19 18:46:17 +0530

    Keywords

---
+++ Level1/q9.java
@@ -0,0 +1,8 @@
+//. Identify and use three Java keywords in a program.
+public class q9 {
+    public static void main(String[] args) {
+        System.out.println("public :It is an access modifier used to make the class accessible from outside the package.");
+        System.out.println("class: It is used to declare a class name.");
+        System.out.println("void is the return type that simply perform some actions.");
+    }
+}
