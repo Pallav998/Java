8f92e080fc1963f41cd552376208833de6dcfb54
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-19 18:39:13 +0530

    Formatted output

---
+++ Level1/q8.java
@@ -0,0 +1,9 @@
+// Demonstrate the use of `System.out.printf` for formatted output.
+public class q8 {
+   public static void main(String[] args) {
+    int a =39;
+    String day = "Monday";
+    System.out.printf("a is : %s & The day is : %s", a ,day);
+    //System.out.printf is a method in Java used for formatted output
+   } 
+}
