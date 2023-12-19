54922c7b38be3bffa4ac7ace3d29db6a3bfd0e26
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-19 18:12:24 +0530

    Java level 1

---
+++ Level1/q1.java
@@ -0,0 +1,14 @@
+//. WAP with both single-line and multi-line comments.and also print helloworld.
+
+/*public: It is an access modifier.
+ * Static: Main method must be static
+ * void:A return type that does'nt return anthing it simply perform some actions
+ * main:It's the method name. Without main method JVM will not execute.
+ * String args[]:It is a method parameter.An array of named args string type.
+ * 
+*/
+public class q1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
