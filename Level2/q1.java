62d743ea778f2a90ae34e5adacfdfe1613aeb519
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-20 22:26:29 +0530

    Fahrenheit to Celsius

---
+++ Level2/q1.java
@@ -0,0 +1,9 @@
+//WAP that converts Fahrenheit to Celsius using appropriate data types.
+public class q1 {
+    public static void main(String[] args) {
+        float Fahrenheit ,Celsius;
+        Fahrenheit =64;
+        Celsius = (Fahrenheit-32)*5/9;
+        System.out.println("Temperature in Celsius : "+Celsius);
+    }
+}
