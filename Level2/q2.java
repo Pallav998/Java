b054d39065bd50214c8a4b4f802fef1f8924ae17
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-20 22:27:14 +0530

    Celsius to fahrenheit

---
+++ Level2/q2.java
@@ -0,0 +1,9 @@
+//WAP that converts Celsius to Fahrenheit using appropriate data types
+public class q2 {
+    public static void main(String[] args) {
+        float F,C;
+        C = 17;
+        F = (C*9/5)+32;
+        System.out.println("Temprature in Fahrenheit is :"+F);
+    }
+}
