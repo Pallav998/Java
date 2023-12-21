b7255bbb12f9e6a07a6d5187fa6eedcb682244cd
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2023-12-21 18:20:34 +0530

    Operator

---
+++ Level2/q5.java
@@ -0,0 +1,18 @@
+//Use arithmetic operators to perform basic calculations
+public class q5 {
+    public static void main(String[] args) {
+        //Airthmetic operators are used to perform airthmetic operations.
+        int a =12;
+        int b =4;
+        int sum = a+b;
+        System.out.println("Sum is :"+sum);
+        int sub = a-b;
+        System.out.println("Subtration is :"+sub);
+        int multy = a*b;
+        System.out.println("Multyplication is :"+multy);
+        int division = a/b;
+        System.out.println("Division is :"+division);
+        int remainder = a%b;
+        System.out.println("Remainder is :"+remainder);
+    }
+}
