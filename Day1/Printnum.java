caf32a8bb580ccb06e4ef5e16ed1553aef90bd80
ThakurPallavi24 <139746038+ThakurPallavi24@users.noreply.github.com>
2024-02-12 11:44:40 +0530

    Print number between 1 to 50

---
+++ Day1/Printnum.java
@@ -0,0 +1,13 @@
+//WAP to print all number between 1 to 50.
+public class Printnum {
+    public static void main(String args[])
+    {
+        System.out.println("The number between 1 to 50 is :" );
+        
+        for(int i = 1 ; i<=50; i++)
+        {
+            System.out.println(i);
+        }
+        
+    }
+}
